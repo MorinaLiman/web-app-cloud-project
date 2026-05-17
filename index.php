76675141276ccab3d7514f22927342b8663370c3
Liman Morina <159453787+MorinaLiman@users.noreply.github.com>
2026-05-17 19:52:35 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -7,26 +7,27 @@ $pageCss = 'index.css';
 $extraPageCss = 'catalog.css';
 $activePage = 'home';
 
-try {
-    $latestStmt = $pdo->query("
-        SELECT 
-            p.id, 
-            p.title, 
-            p.author, 
-            p.price, 
-            p.image_url, 
-            c.name AS category_name, 
-            p.description 
-        FROM products p 
-        LEFT JOIN categories c ON p.category_id = c.id 
-        ORDER BY p.created_at DESC 
-        LIMIT 6
-    ");
+$sql = "
+    SELECT 
+        p.id, 
+        p.title, 
+        p.author, 
+        p.price, 
+        p.image_url, 
+        c.name AS category_name, 
+        p.description 
+    FROM products p 
+    LEFT JOIN categories c ON p.category_id = c.id 
+    ORDER BY p.created_at DESC 
+    LIMIT 6
+";
 
-    $latestBooks = $latestStmt->fetchAll();
+$result = mysqli_query($conn, $sql);
 
-} catch (PDOException $e) {
-    die("Query error: " . $e->getMessage());
+if (!$result) {
+    die("Query error: " . mysqli_error($conn));
 }
+
+$latestBooks = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 include __DIR__ . '/views/index.view.php';
